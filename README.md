def6a248e039b110cad55ca9c25d907997873160
smoming <smoming1031@gmail.com>
2021-01-23 23:42:56 +0800

    handle CROS

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # NiCatApp_DONETCORE
 
 # Json Package
+
 dotnet add package System.Text.Json
 
 # MySql Connection Package
+
 dotnet add package MySql.Data
 
 # Daper - ORM Package
+
 dotnet add package Dapper
+
+# CROS
+
+dotnet add package Microsoft.AspNetCore.Cors
--- Startup.cs
+++ Startup.cs
@@ -12,48 +12,50 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace NiCatApp_DONETCORE
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace NiCatApp_DONETCORE {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NiCatApp_DONETCORE", Version = "v1" });
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddCors (options => {
+                // CorsPolicy 是自訂的 Policy 名稱
+                options.AddPolicy ("CorsPolicy", policy => {
+                    policy.WithOrigins ("http://localhost:4200")
+                        .AllowAnyHeader ()
+                        .AllowAnyMethod ()
+                        .AllowCredentials ();
+                });
             });
-            services.AddTransient<DbConnection>(_ => new DbConnection(Configuration["ConnectionStrings:MYSQL"]));
+            services.AddControllers ();
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new OpenApiInfo { Title = "NiCatApp_DONETCORE", Version = "v1" });
+            });
+            services.AddTransient<DbConnection> (_ => new DbConnection (Configuration["ConnectionStrings:MYSQL"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NiCatApp_DONETCORE v1"));
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+                app.UseSwagger ();
+                app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "NiCatApp_DONETCORE v1"));
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection ();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthorization();
+            app.UseCors ("CorsPolicy");
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseAuthorization ();
+
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
