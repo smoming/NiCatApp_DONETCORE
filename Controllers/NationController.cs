992b03db90c646abf70f16d108115988a1e50b06
smoming <smoming1031@gmail.com>
2021-01-24 00:05:54 +0800

    add nation function - get, add, update, delete

--- Controllers/NationController.cs
+++ Controllers/NationController.cs
@@ -1,26 +1,43 @@
 using System.Collections.Generic;
 using Dapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using NiCatApp_DONETCORE.Models;
 using NiCatApp_DONETCORE.Services;
 
-namespace NiCatApp_DONETCORE.Controllers
-{
+namespace NiCatApp_DONETCORE.Controllers {
     [ApiController]
-    [Route("api/ApiNation")]
-    public class NationController
-    {
-        private DbConnection _conn;
-        public NationController(DbConnection conn)
-        {
-            _conn = conn;
+    [Route ("api/ApiNation")]
+    public class NationController {
+        private NationService svc;
+        public NationController (DbConnection conn) {
+            svc = new NationService (conn);
         }
 
-        public IEnumerable<NationDTO> list()
-        {
-            var svc = new NationService(_conn);
-            return svc.list();
+        [HttpGet]
+        public IEnumerable<NationDTO> list () {
+            return svc.list ();
+        }
+
+        [HttpGet ("{id}")]
+        public NationDTO get (string id) {
+            return svc.get (id);
+        }
+
+        [HttpPost]
+        public void add ([FromBody] NationDTO item) {
+            svc.add (item);
+        }
+
+        [HttpPut ("{id}")]
+        public void update (string id, [FromBody] NationDTO item) {
+            svc.update (item);
+        }
+
+        [HttpDelete ("{id}")]
+        public void delete (string id) {
+            svc.delete (id);
         }
     }
 }
